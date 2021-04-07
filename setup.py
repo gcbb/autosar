21de1e8a444fa2a402de6fc1fb6ce1d8f6cb43b3
cogu <congus8@gmail.com>
2021-04-07 10:45:52 +0200

    Add new Amber Python package
    Change autosar package version to v0.5.0

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ def readme():
         return f.read()
 
 setup(name='autosar',
-      version='0.4.0',
+      version='0.5.0',
       description='autosar python module',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Text Processing :: Markup :: XML',
       ],
       url='http://github.com/cogu/autosar',
@@ -20,9 +20,9 @@ setup(name='autosar',
       author_email='congus8@gmail.com',
       license='MIT',
 	  install_requires=[
-          'cfile>=0.1.4',
+          'cfile>=0.2.0',
       ],
-      packages=['autosar','autosar.parser','autosar.writer','autosar.rte', 'autosar.bsw', 'autosar.util'],
-	  dependency_links=['https://github.com/cogu/cfile/archive/v0.1.5.tar.gz#egg=cfile-0.1.5'],
+      packages=['autosar','autosar.parser', 'autosar.template', 'autosar.writer', 'autosar.util',
+        'amber', 'amber.rte', 'amber.rte.generator', 'amber.rte.generator', 'amber.template', 'amber.template.services'],
 	  zip_safe=False,
 	  test_suite='tests.my_test_suite')
